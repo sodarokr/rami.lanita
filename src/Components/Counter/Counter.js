7f03589b197cd1c045743b9703af30d2b42f6ccc
Juan Manuel Gimenez <juanmanuelgimenez.00@gmail.com>
2022-06-05 15:58:15 -0300

    arregladas las imagenes del Counter

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -14,11 +14,17 @@ const Counter = ({ valorInicial, stock }) => {
   return (
     <div className="Counter">
       <button className="Counter__boton" onClick={incrementar}>
-        <img className="Counter__boton-icon" src="./mas.png" />
+        <img
+          className="Counter__boton-icon"
+          src={process.env.PUBLIC_URL + "/mas.png"}
+        />
       </button>
       <p className="Counter__cantidad">{contador}</p>
       <button className="Counter__boton" onClick={decrementar}>
-        <img className="Counter__boton-icon" src="./menos.png" />
+        <img
+          className="Counter__boton-icon"
+          src={process.env.PUBLIC_URL + "/menos.png"}
+        />
       </button>
     </div>
   );
